40d1acb0ead946504703ac3700b61d1d3bcd9c7b
Jeff Dyer <jeffdyer@acm.org>
2024-08-16 12:20:19 -0700

    Cleanup

--- packages/app/lib/components/form/Form.tsx
+++ packages/app/lib/components/form/Form.tsx
@@ -1,57 +1,6 @@
 import "../../index.css";
 import { useEffect, useState } from 'react';
 
-// function renderJSON(data) {
-//   delete data.schema;
-//   return (
-//     <pre className="text-xs">{JSON.stringify(data, null, 2)}</pre>
-//   );
-// }
-
-// function render({ state }) {
-//   const { data } = state;
-//   return renderJSON(data);
-// }
-
-// const callbacks = {
-//   errorListener: function(e) {
-//     // Adds a listener to all error codes.
-//     console.log("Error Code ", e.code);
-//     console.log("Error Message ", e.msg);
-//     console.log("Error Detail ", e.detail);
-//   },
-//   /* example object
-//      {
-//      code:   10001,
-//      msg:    "JSON format error",
-//      detail: "Activity JSON Poorly formed. Incorrect activity type found: abcd",
-//      }
-//   */
-
-//   readyListener: function() {
-//     console.log("Learnosity Questions API is ready");
-//   },
-
-//   labelBundle: {
-//     loadingInfo: "Loading page...",
-//     play: "play",
-//   },
-
-//   saveSuccess: function(response_ids) {
-//     for(let i = 0; i < response_ids.length; i++) {
-//       console.log("Responses saved : ", response_ids[i]);
-//     }
-//   },
-
-//   saveError: function(e) {
-//     console.log("Save failed - error ", e);
-//   },
-
-//   saveProgress: function(progress) {
-//     console.log("Save progress - ", progress);
-//   }
-// };
-
 export const Form = ({ state }) => {
   const [scriptLoaded, setScriptLoaded] = useState(false);
   const request = state.data;
